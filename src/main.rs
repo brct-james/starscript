5b2501d9c5bb10a02b140298b259676e449b7ce6
brct-james <james.clague@brct.io>
2022-03-11 17:56:48 +0000

    Add /my/agent

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use spacedustrs::client::Client;
+// use std::collections::HashMap;
+// use std::thread;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+
+    // Setup Game Client
+    let http_client = spacedustrs::client::get_http_client(None);
+
+    // Register agent
+    // let claim_agent_response = spacedustrs::client::claim_agent(
+    //     http_client,
+    //     "1test699".to_string(),
+    //     "COMMERCE_REPUBLIC".to_string(),
+    // )
+    // .await;
+
+    // match claim_agent_response {
+    //     Ok(res) => {
+    //         println!("{:#?}", res);
+    //     }
+    //     Err(res_err) => {
+    //         println!("{:#?}", res_err);
+    //     }
+    // }
+
+    let client = Client::new(
+        http_client,
+        "GREEN".to_string(),
+        "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWVyIjoiR1JFRU4iLCJpYXQiOjE2NDcwMTY1NjMsInN1YiI6ImFnZW50LXRva2VuIn0.S-9AfG_asd21tsdGf9TF-cwML32x-TFd-b2n9WT21CKA3gkS9qhR15Zng9I2chv92NRriUGUDVb3flc-nZfnbDrMK_iBUbHT7oLiUu1X4Rr9HumsHUdSEltpVGxTvRm6-0udRgLuy9ndoXCxomUsTruszqdRZ5BJb9-2OYcP_kU6FnYcERDGoKNn6jISmPCaSnSs8nCDw5dbSrDF16mAJiGozJlx9j1gDUHWzeQZF7k4fonPxcLPGQjSa4mKIMaYYCh5oATW3wMh5qnXb-iz-wiwHZ7aXd1jkmDVQzeFXYqLpNf1jjQOXXdqEcZ_lFe79Mgeg1vuNtJDZpPNh-KC7P7YdC_F-7DYA82x6uYDPN8bwxcPd5uNmw0lZr5_C0lUI_z8-igPurxDBLizwjBdMdjIaqY2YSjEV_zocRy-I-N_0c43Dc9a5zZoFFH0DPwFrR2c9pp3tSkFsRMHp86SVlASIDXCQlgLvlNoDORi79dVR9ap64JgK3z-ttoJ_v90".to_string(),
+    );
+
+    match client.get_my_agent_details().await {
+        Ok(res) => {
+            println!("{:#?}", res);
+        }
+        Err(res_err) => {
+            println!("{:#?}", res_err);
+        }
+    }
+
+    Ok(())
+}
